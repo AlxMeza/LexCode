af89695a7a97011605e57e22aeaf63afa86cb61a
AlxMeza <alexis.meza24421@gmail.com>
2023-04-13 12:47:27 -0600

    update technologies component in portfolio

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,11 +11,11 @@ export default function NavBar () {
     <>
       <div className="sticky top-0 border-b-2 border-[#bd93f9] z-10">
         <nav className="bg-[#282a36] border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-[#282a36]">
-          <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <div className="container flex flex-wrap mx-auto justify-between">
             <a href="#" className="flex items-center">
                 <img src="/logo.svg" alt="LexCode" className="pl-3 h-8" />
             </a>
-            <button className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-200 dark:hover:bg-gray-800 dark:focus:ring-[#50fa7b]" 
+            <button className="inline-flex justify-items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-200 dark:hover:bg-gray-800 dark:focus:ring-[#50fa7b]" 
               onClick={() => setOpenNavbar(!openNavbar)}>
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
             </button>
