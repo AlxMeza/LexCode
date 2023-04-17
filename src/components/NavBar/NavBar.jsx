ff92f58ea6b9821f0d8fd025a1627dd772013a72
AlxMeza <alexis.meza24421@gmail.com>
2023-04-17 16:50:16 -0600

    modify navbar compnent and add new background

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,16 +10,16 @@ export default function NavBar () {
   return (
     <>
       <div className="sticky top-0 border-b-2 border-[#bd93f9] z-10">
-        <nav className="bg-[#282a36] border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-[#282a36]">
-          <div className="container flex flex-wrap mx-auto justify-between">
+        <nav className="bg-[#282a36] border-gray-200 py-2.5 dark:bg-[#282a36] md:px-10 px-5">
+          <div className="flex flex-wrap w-full">
             <a href="#" className="flex items-center">
                 <img src="/logo.svg" alt="LexCode" className="pl-3 h-8" />
             </a>
-            <button className="inline-flex justify-items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-200 dark:hover:bg-gray-800 dark:focus:ring-[#50fa7b]" 
+            <button className="inline-flex justify-items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-200 dark:hover:bg-gray-800 dark:focus:ring-[#50fa7b] ml-auto" 
               onClick={() => setOpenNavbar(!openNavbar)}>
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
             </button>
-            <div className={openNavbar ? "flex w-full md:block md:w-auto" : "hidden w-full md:block md:w-auto"} >
+            <div className={openNavbar ? "flex w-full md:block md:w-auto ml-auto" : "hidden w-full md:block md:w-auto ml-auto"} >
               <ul className="flex flex-col p-4 mt-4 bg-gray-[#44475a] rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-[#282a36] md:dark:bg-[#282a36] dark:border-[#282a36]">
                 <li>
                   <a href="#Home" name='home' className={styles[btnStates.home]} onClick={() => {
